997701e4ee9ac24c45521b22a62b4949cfcbe0f8
Matt Ellig <matt.ellig@gmail.com>
2021-04-10 22:07:38 -0400

    Fix client ESLint integration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
     server: {
         open: true,
     },
-    plugins: [reactRefresh()],
+    plugins: [
+        eslintPlugin({ include: 'src/**/*.{js,jsx}' }),
+        reactRefresh()
+    ],
 });
